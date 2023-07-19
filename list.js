787cc4bc9c4b6325468b1721c0f0d674d0ff8869
Kimhyeseung <hg724932@gmail.com>
2023-07-19 14:19:15 +0900

    Todolist

---
+++ list.js
@@ -0,0 +1,48 @@
+const btn = document.getElementById("btn");
+let addValue = document.getElementById("addValue");
+let result = document.getElementById("result");
+
+//할일 추가하기
+function addTodo(){
+    if (addValue.value == false){
+        alert("내용을 입력하세요!!")
+    }else{
+        let list = document.createElement("li");
+        let del = document.createElement("button");
+        list.innerHTML = addValue.value;
+        result.appendChild(list);
+        list.appendChild(del);
+        del.innerText = "X";
+        del.style.fontSize = "20px";
+        del.style.border = "none";
+        del.style.float = "right";
+        del.style.right = "17px";
+        del.style.marginTop = "10px";
+        del.style.cursor = "pointer";
+        del.addEventListener("click" , deleteList);
+        del.style.position = "relative";
+        addValue.value = "";
+        addValue.focus();
+        list.addEventListener("click" , function(){
+            list.style.textDecoration = "line-through";
+            list.style.color = "red";
+        });
+    }
+}
+//할일 목록 삭제시
+function deleteList(e){
+   let removeOne = e.target.parentElement;
+   removeOne.remove();
+}
+//전체 삭제시
+function allClearList(e){
+    if(confirm("정말 삭제하시겠습니까?") == true){
+        if(result.innerHTML == ""){
+            alert("삭제할 목록이 없습니다.")
+        }else{
+            result.innerText = ""; //전체 삭제
+        }
+    }else{
+            return false; //삭제 취소
+        }
+    }
